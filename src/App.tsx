b8f414884dd2851dd5115dcc185cad2f42eb01af
karsh <utkarsh@cyberfear.com>
2026-01-02 10:55:51 +1100

    fix: infite search loop

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import ScrollToTop from "./components/ScrollToTop";
 import Index from "./pages/Index";
@@ -34,10 +34,7 @@ const App = () => (
           <Route path="/opennetics" element={<Opennetics />} />
           <Route path="/journal" element={<Journal />} />
           <Route path="/journal/heist-aftermath" element={<JournalEntry1 />} />
-          <Route
-            path="/journal/chrome-installation"
-            element={<JournalEntry2 />}
-          />
+          <Route path="/journal/chrome-installation" element={<JournalEntry2 />} />
           <Route path="/dotfiles" element={<Dotfiles />} />
           <Route path="/pgp" element={<PGP />} />
           <Route path="*" element={<NotFound />} />
