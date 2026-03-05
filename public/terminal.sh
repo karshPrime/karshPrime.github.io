b8da6dd7fe4f64e52effa070e8a742f6e9f86bba
karsh <utkarsh@cyberfear.com>
2026-03-06 08:18:53 +1100

    interface

--- public/terminal.sh
+++ public/terminal.sh
@@ -10,13 +10,31 @@ echo "\
 "
 }
 
+all_commands() {
+echo "\
+List Commands:
+   list systems                List all configuration options.
+   list scripts                List all available scripts.
+   list tools                  List all tools supported with a configuration.
+
+Install Services:
+   add script {script, ...}    Install the specified scripts.
+   add system (system)         Install the specified configuration from the defined list.
+   add config {tool, ...}      Install configurations for the specified tool.
+
+Misc:
+   about                       Display all information about the system.
+   help                        Show all available commands.
+   quit                        Exit the application."
+}
+
 
 #- banner ------------------------------------------------------------------------------------------
 
-echo
+clear
 colour_line
 echo "\
-   _    _   ______  _        _        ______      _    __  ______   ______   ______   _    _ 
+   _    _   ______  _        _        ______      _    __  ______   ______   ______   _    _
   | |  | | | |     | |      | |      / |  | \    | |  / / | |  | | | |  | \ / |      | |  | |
   | |--| | | |---- | |   _  | |   _  | |  | |    | |-< <  | |__| | | |__| | '------. | |--| |
   |_|  |_| |_|____ |_|__|_| |_|__|_| \_|__|_/    |_|  \_\ |_|  |_| |_|  \_\  ____|_/ |_|  |_|
@@ -25,4 +43,25 @@ colour_line
 
 
 #---------------------------------------------------------------------------------------------------
+
+while true; do
+    echo
+    read -p "> " user_input
+
+    # Check if the user wants to exit
+    if [[ "$user_input" == "x" || "$user_input" == "quit" || "$user_input"  == "exit" ]]; then
+        clear
+        colour_line
+        echo "\nGoodbye Karsh..."
+        sleep 1
+        break
+
+    elif [[ "$user_input" == "help" || "$user_input" == "?" ]]; then
+        all_commands
+
+    elif [[ "$user_input" == "clear" ]]; then
+        clear
+        colour_line
+    fi
+done
 
